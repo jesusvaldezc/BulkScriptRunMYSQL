2381a39171f49b5c8198036b49e456f5c85de7ce
jesusvaldezc <49350579+jesusvaldezc@users.noreply.github.com>
2022-02-15 13:20:45 -0600

    Add files via upload

--- bulkmysql.py
+++ bulkmysql.py
@@ -10,56 +10,35 @@ import pandas as pd
 import mysql.connector
 import pandas
 import base64, io
+import sqlalchemy
 
 cwd = os.getcwd()
 
-db = mysql.connector.connect(
-  host="127.0.0.1",
-  user="root",
-  password="admin",
-  database="testpython"
-)
-
-consts = ['wtpart', 
-         'wtpartmaster', 
-         'wtpartusagelink', 
-         'att_wtpart', 
-         'epmdoocument', 
-         'epmdocumentmaster']
+db = sqlalchemy.create_engine('mysql://root:admin@127.0.0.1:3306/testpython')
 
 with open(cwd + '\\Query.sql') as file:
         queries = file.read().split(';')
 
-cursor = db.cursor(buffered=True)
 
 while len(queries) > 1:
 
     actualQuery = queries.pop(0)
-    actualQuery = actualQuery.replace('\n', ' ')
-    actualQuery = actualQuery.replace('\t', ' ')
+    #actualQuery = actualQuery.replace('\n', ' ')
+    #actualQuery = actualQuery.replace('\t', ' ')
     actualQuery = actualQuery.strip()
+    tableName = actualQuery.rsplit('.')[1]
+    
+    try:
+        db.execute(actualQuery)
+        print('Running query... ', actualQuery)
+        df = pd.read_sql(actualQuery, con=db)
+        #print(df)
+        #df = pd.DataFrame(cursor.fetchall())
+        #df = df.iloc[: , 1:]
+        compression_opts = dict(method='zip', archive_name= tableName + '.csv')  
+        df.to_csv(tableName + '.zip', index=False, compression=compression_opts, sep='|', header=True, encoding='utf-16', quoting=QUOTE_ALL)
+    except (db.Error, db.Warning) as e:
+        print(e)
+        sys.exit(1)
 
-    if actualQuery.startswith('SELECT'):
-        for i in consts:
-            if actualQuery.endswith(i):
-                try:
-                    cursor.execute(actualQuery)
-                    print('Running query... ', cursor.statement)
-                    df = pd.read_sql(actualQuery, con=db)
-                    #print(df)
-                    #df = pd.DataFrame(cursor.fetchall())
-                    #df = df.iloc[: , 1:]
-                    compression_opts = dict(method='zip', archive_name= i + '.csv')  
-                    df.to_csv(i + '.zip', index=False, compression=compression_opts, sep='|', header=True, encoding='utf-16', quoting=QUOTE_ALL)
-                except (db.Error, db.Warning) as e:
-                    print(e)
-                    sys.exit(1)
-    else:
-        try:
-            cursor.execute(actualQuery)
-            print('Running query... ', cursor.statement)
-            db.commit()
-        except (mysql.connector.Error, mysql.connector.Warning) as e:
-            print(e)
-            sys.exit(1)
         
